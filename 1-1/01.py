264e6cd03543093fc942db02de4132b1998730e2
btbrad <24836323+btbrad@users.noreply.github.com>
2024-12-09 21:19:23 +0800

    restart

---
+++ 1-1/01.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.showturtle()
+
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(300)
+turtle.goto(0,0)
+
+turtle.penup()
+turtle.goto(-100, 100)
+turtle.pendown()
+turtle.color("red")
+turtle.circle(50)
+turtle.done()
---
+++ 1-1/olympic-ring.py
@@ -0,0 +1,33 @@
+import turtle
+
+turtle.showturtle()
+turtle.pensize(10)
+
+turtle.color("blue")
+turtle.circle(100)
+
+turtle.penup()
+turtle.goto(220, 0)
+turtle.pendown()
+turtle.color("black")
+turtle.circle(100)
+
+turtle.penup()
+turtle.goto(440, 0)
+turtle.pendown()
+turtle.color("red")
+turtle.circle(100)
+
+turtle.penup()
+turtle.goto(110, -100)
+turtle.pendown()
+turtle.color("yellow")
+turtle.circle(100)
+
+turtle.penup()
+turtle.goto(330, -100)
+turtle.pendown()
+turtle.color("green")
+turtle.circle(100)
+
+turtle.done()
