1c549cb1e463e27b8113da941db002576a2d25cc
btbrad <24836323+btbrad@users.noreply.github.com>
2024-12-12 22:04:52 +0800

    判断闰年

--- 1-3/02.py
+++ 1-3/02.py
@@ -1,3 +1,12 @@
 """
 输入圆的半径计算计算周长和面积。
 """
+import math
+
+radius = float(input("请输入半径："))
+perimeter = 2 * math.pi * radius
+area = math.pi * radius**2
+print(f"""
+周长：{perimeter: .2f},
+面积：{area: .2f}
+""")
