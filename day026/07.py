32e1a151394ab2680fa71215c471ce58a7c8d84f
btbrad <24836323+btbrad@users.noreply.github.com>
2025-03-21 22:50:22 +0800

    函数

---
+++ day026/07.py
@@ -0,0 +1,13 @@
+def search4letters(phrase: str, letters: str = 'aeiou') -> set:
+    """Return a set of the 'letters' found in phrase"""
+    # vowels = ("a", "e", "i", "o", "u")
+    # word = set(input("Provide a word to search for vowels:"))
+    # found = set(vowels).intersection(set(word))
+
+    # for vowel in found:
+    #     print(vowel)
+    return set(letters).intersection(set(phrase))
+
+
+print(search4letters('hitch-hiker'))
+print(search4letters(phrase='apple', letters='aiu'))
