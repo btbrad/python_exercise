20c931bcced491cfeada540424ffb4c4b1e869e4
btbrad <biantaobrad@gmail.com>
2025-03-24 23:03:17 +0800

    flask项目

--- day028/webapp/vsearch4web.py
+++ day028/webapp/vsearch4web.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
 
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html', the_title='Here are your results', the_phrase=phrase, the_letters=letters, the_results=results)
+
 @app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask'
-
-@app.route('/search4')
-def do_search() -> str:
-    return str(search4letters('life, the universe, and everything!', 'eiru,!'))
-
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
