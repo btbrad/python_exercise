133a26bec30b4ccf20fe1a99b5e5312b6ca62281
btbrad <biantaobrad@gmail.com>
2025-04-10 22:07:09 +0800

    数据类型

---
+++ day039/01.py
@@ -0,0 +1,18 @@
+name = 'michael jordan'
+
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+language = '  python    '
+
+print(language.strip())
+
+print(8/4) # 浮点型
+print(f'{0.2 + 0.1:.2f}')
+a = 3 * 0.1
+print(a)
+print(round(a, 2))
+
+universe_age = 14_000_000_000
+print(universe_age)
