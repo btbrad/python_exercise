364fbf4fbf497f503ca9ae15471f70fd3078ede1
btbrad <biantaobrad@gmail.com>
2025-04-25 22:13:30 +0800

    flask操作session

--- day053/02.py
+++ day053/02.py
@@ -1,6 +1,8 @@
-from flask import Flask, make_response, request
+from flask import Flask, make_response, request, session
 
 app = Flask(__name__)
+
+app.secret_key = 'i am a key'
 
 @app.route('/')
 def index():
@@ -16,6 +18,16 @@ def set_cookie():
 def get_cookie():
     foo = request.cookies.get('foo')
     return f'get cookie: foo={foo}'
+
+@app.route('/set-session/')
+def set_session():
+    session['foo'] = 'bar'
+    return '设置了一个session'
+
+@app.route('/get-session/')
+def get_session():
+    foo = session['foo']
+    return f'读取了一个session: { foo }'
 
 if __name__ == '__main__':
     app.run(debug=True)
