ced08481f277fae89c8f7df82102a917058567a3
btbrad <biantaobrad@gmail.com>
2025-05-24 13:45:18 +0800

    flask拦截器

---
+++ day063/01.py
@@ -0,0 +1,34 @@
+from flask import Flask, session, request
+
+app = Flask(__name__)
+
+app.secret_key = 'secret'
+@app.before_request
+def before_request():
+  url = request.path
+  print(url)
+  if url in ['/login/']:
+    pass
+  else:
+    if session.get('isLogin'):
+      pass
+    else:
+      return '请登录'
+
+@app.route('/login/')
+def login():
+  session['isLogin'] = True
+  return '登录成功！'
+
+@app.route('/logout/')
+def logout():
+  session['isLogin'] = False
+  return '退出登录！'
+
+@app.route('/')
+def index():
+  return 'Hello Flask !'
+
+if __name__ == '__main__':
+  app.run(debug=True)
+
