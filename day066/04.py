3b1139e846a7cb2245f8e2b240d2f983f4bd37b7
btbrad <biantaobrad@gmail.com>
2025-06-10 21:09:47 +0800

    级联数据操作

---
+++ day066/04.py
@@ -0,0 +1,51 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, backref
+from db_util import Base, Session
+
+
+class User(Base):
+    __tablename__ = 't_user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(32))
+    # articles = relationship('Article', backref='user', cascade='save-update, delete, delete-orphan', single_parent=True)
+
+
+class Article(Base):
+    __tablename__ = 't_article'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(32))
+    uid = Column(Integer, ForeignKey('t_user.id'))
+
+    user = relationship('User', backref=backref('articles',cascade='save-update, delete, delete-orphan'))
+
+
+def create_data():
+    Base.metadata.drop_all()
+    Base.metadata.create_all()
+    user = User(name='alex')
+    art1 = Article(title='article1')
+    art2 = Article(title='article2')
+    user.articles.append(art1)
+    user.articles.append(art2)
+
+    with Session() as session:
+        session.add(user)
+        session.commit()
+
+def delete_data():
+    with Session() as session:
+        user = session.query(User).first()
+        session.delete(user)
+        session.commit()
+
+def update_data():
+    with Session() as session:
+        user = session.query(User).first()
+        user.articles = []
+        session.commit()
+
+if __name__ == '__main__':
+    # create_data()
+    # delete_data()
+    update_data()
+    
