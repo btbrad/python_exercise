eec9639253a2704ae5aaf0ee119c22f9177d7006
btbrad <biantaobrad@gmail.com>
2025-08-15 16:01:42 +0800

    python变量

---
+++ day073/01.py
@@ -0,0 +1,16 @@
+'''
+  python中的类型转换操作
+'''
+a = 100
+b = 12.345
+c = 'hello world'
+d = True
+
+print(float(a))
+str_b = str(b)
+print(type(str_b))
+
+print(bool(c))
+print(int(d))
+print(chr(97))
+print(ord('a'))
