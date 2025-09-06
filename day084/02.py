bd77c810606e042c77a4f6f72fff85a4d9951138
btbrad <biantaobrad@gmail.com>
2025-09-06 13:56:51 +0800

    判断奇偶

---
+++ day084/02.py
@@ -0,0 +1,10 @@
+def even_or_odd():
+    num = int(input('请输入一个整数：'))
+    if num % 2 == 0:
+        print('偶数')
+    else:
+        print('奇数')
+
+
+if __name__ == "__main__":
+    even_or_odd()               
