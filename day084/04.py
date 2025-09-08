87b1aee1b00a28d426d257d6277dc5945e96c8f4
btbrad <biantaobrad@gmail.com>
2025-09-08 10:39:29 +0800

    格式化

--- day084/04.py
+++ day084/04.py
@@ -1,20 +1,21 @@
-'''
+"""
 题目 3：九九乘法表
 
 打印标准的 九九乘法表，格式如下：
 
-1*1=1  
-1*2=2  2*2=4  
-1*3=3  2*3=6  3*3=9  
-...  
-'''
+1*1=1
+1*2=2  2*2=4
+1*3=3  2*3=6  3*3=9
+...
+"""
+
 
 def multiplication_table():
-  for i in range (1,10):
-    for j in range(1, i +1):
-      print(f'{j}*{i}={i*j}', end='\t')
-    print('')
+    for i in range(1, 10):
+        for j in range(1, i + 1):
+            print(f"{j}*{i}={i*j}", end="\t")
+        print("")
 
 
 if __name__ == "__main__":
-  multiplication_table()      
+    multiplication_table()
